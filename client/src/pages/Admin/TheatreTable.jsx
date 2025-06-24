5e8b917dd05e6ce6c80dd7c2e96f54553b935ff1
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-06-24 10:29:33 +0530

    implemented theatreTable in frontend

--- client/src/pages/Admin/TheatreTable.jsx
+++ client/src/pages/Admin/TheatreTable.jsx
@@ -1,11 +1,119 @@
-import { Table } from 'antd'
+import { useState, useEffect } from "react";
+import { getAllTheatres, updateTheatre } from "../../api/theatre";
+import { showLoading, hideLoading } from "../../redux/loaderSlice";
+import { useDispatch } from "react-redux";
+import { message, Button, Table } from "antd";
 
-const TheatreTable = () => {
+const TheatresTable = () => {
+    const [theatres, setTheatres] = useState([]);
+    const dispatch = useDispatch();
+
+    const getData = async () => {
+        try {
+            dispatch(showLoading());
+            const response = await getAllTheatres();
+            if (response.success) {
+                const allTheatres = response.data;
+                setTheatres(
+                    allTheatres.map(function (item) {
+                        return { ...item, key: `theatre${item._id}` };
+                    })
+                );
+            } else {
+                message.error(response.message);
+            }
+            dispatch(hideLoading());
+        } catch (err) {
+            dispatch(hideLoading());
+            message.error(err.message);
+        }
+    };
+
+    const handleStatusChange = async (theatre) => {
+        try {
+            dispatch(showLoading());
+            let values = {
+                ...theatres,
+                isActive: !theatre.isActive,
+            };
+            const response = await updateTheatre(theatre._id, values);
+            console.log(response, theatre);
+            if (response.success) {
+                message.success(response.message);
+                getData();
+            }
+            dispatch(hideLoading());
+        } catch (err) {
+            dispatch(hideLoading);
+            message.error(err.message);
+        }
+    };
+
+    const columns = [
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Address",
+            dataIndex: "address",
+            key: "address",
+        },
+        {
+            title: "Owner",
+            dataIndex: "owner",
+            render: (text, data) => {
+                return data.owner && data.owner.name;
+            },
+        },
+        {
+            title: "Phone Number",
+            dataIndex: "phone",
+            key: "phone",
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            key: "email",
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
+            render: (status, data) => {
+                if (data.isActive) {
+                    return "Approved";
+                } else {
+                    return "Pending/ Blocked";
+                }
+            },
+        },
+        {
+            title: "Action",
+            dataIndex: "action",
+            render: (text, data) => {
+                return (
+                    <div className="d-flex align-items-center gap-10">
+                        <Button onClick={() => handleStatusChange(data)}>
+                            {data.isActive ? "Block" : "Approve"}
+                        </Button>
+                    </div>
+                );
+            },
+        },
+    ];
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
-        <div>
-            <Table />
-        </div>
-    )
-}
+        <>
+            {theatres && theatres.length > 0 && (
+                <Table dataSource={theatres} columns={columns} />
+            )}
+        </>
+    );
+};
 
-export default TheatreTable
+export default TheatresTable;
